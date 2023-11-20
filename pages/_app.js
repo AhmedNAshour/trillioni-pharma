b7e97810a345a269debe97e8922813279cb66721
AhmedNAshour <ahmed.nasr.ashour@gmail.com>
2023-11-20 18:02:59 +0200

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
-import '../public/assets/css/style.css'
-import '../public/assets/css/modal.css'
-import "../public/assets/css/swiper-custom.css";
-import React, { useEffect, useState } from "react";
+import '../public/assets/css/style.css';
+import '../public/assets/css/modal.css';
+import '../public/assets/css/swiper-custom.css';
+import React, { useEffect, useState } from 'react';
 import Preloader from '../components/elements/Preloader';
+import { Roboto } from '@next/font/google';
+
+const roboto = {
+  weight: ['400', '700'],
+};
 
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(false);
@@ -12,21 +17,22 @@ function MyApp({ Component, pageProps }) {
       setLoading(false);
     }, 2000);
 
-    if (typeof window !== "undefined") {
-      window.WOW = require("wowjs");
+    if (typeof window !== 'undefined') {
+      window.WOW = require('wowjs');
     }
     new WOW.WOW().init();
   }, []);
   return (
     <>
       {!loading ? (
-        <Component {...pageProps} />
+        <main className={roboto.className}>
+          <Component {...pageProps} />
+        </main>
       ) : (
         <Preloader />
       )}
-
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
